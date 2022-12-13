1fef48231c54be6a2f56682e89843d088716f3ba
Matthew Creek <matthew.creek27@gmail.com>
2022-12-12 19:28:53 -0600

    added crew mutability function on display

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
+  memberBeingEdited: object = null;
+
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
@@ -18,4 +20,18 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) :void {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+  remove(member: object) :void {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  edit(member:object) :void {
+    this.memberBeingEdited = member;
+  }
+  save(name:string, member: object) :void {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
 }
