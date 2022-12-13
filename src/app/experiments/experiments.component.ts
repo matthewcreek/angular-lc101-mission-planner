0f568541ff0c861ff62350e60d7b631aa8b3c610
Matthew Creek <matthew.creek27@gmail.com>
2022-12-12 21:20:46 -0600

    bonus missions 5 through 7

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = ['Mars soil sample','Plant growth in habitat','Human bone density'];
+  itemBeingEdited: string = null;
+
   constructor() { }
 
   ngOnInit() {
   }
+  add(item: string) {
+    if (this.experiments.some((element: string) => element === item)){
+      return;
+    } else this.experiments.push(item)
+  }
 
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+
+  remove(item: string) {
+    let index = this.experiments.indexOf(item);
+    this.experiments.splice(index, 1);
+  }
+
+  save(item: string) {
+    let index = this.experiments.indexOf(this.itemBeingEdited)
+    this.experiments[index] = item;
+  }
 }
