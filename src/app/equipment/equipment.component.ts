2a2dfc978a2488dd90dc6f001fc9c1a7e95c6456
Matthew Creek <matthew.creek27@gmail.com>
2022-12-12 21:16:45 -0600

    bonus missions 2 through 4

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  itemBeingEdited: string = null;
+
+  equipment: string[] = ['Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks']
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(item: string) {
+    if (this.equipment.some((element: string) => element === item)){
+      return;
+    } else this.equipment.push(item)
+  }
+
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+
+  remove(item: string) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+
+  save(item: string) {
+    let index = this.equipment.indexOf(this.itemBeingEdited)
+    this.equipment[index] = item;
+  }
 }
